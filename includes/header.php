812733060b33521dc61b27ed4fbc6aa90b7c4292
RPZXD <rpz.tiwa@phichai.ac.th>
2025-04-30 17:21:49 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -13,34 +13,34 @@
     <!-- Google Font: Mali -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mali:wght@200;300;400;500;600;700&display=swap">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/../plugins/fontawesome-free/css/all.min.css">
     <!-- DataTables -->
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="/../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="/../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="/../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="/../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="/../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/jqvmap/jqvmap.min.css">
+    <link rel="stylesheet" href="/../plugins/jqvmap/jqvmap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="/club.phichai.ac.th/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="/../dist/css/adminlte.min.css">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="/../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="/../plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
-    <link rel="stylesheet" href="/club.phichai.ac.th/plugins/summernote/summernote-bs4.min.css">
-    <link rel="icon" type="image/png" href="/club.phichai.ac.th/dist/img/logo-phicha.png" />
+    <link rel="stylesheet" href="/../plugins/summernote/summernote-bs4.min.css">
+    <link rel="icon" type="image/png" href="/../dist/img/logo-phicha.png" />
     <!-- SweetAlert2 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- jQuery UI -->
-    <script src="/club.phichai.ac.th/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="/../plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- jQuery UI CSS (optional, for default styling) -->
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
