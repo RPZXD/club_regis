a0307c58b2a948d5a7a5154468b165c841d91800
RPZXD <rpz.tiwa@phichai.ac.th>
2025-05-01 01:55:03 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/controllers/UserController.php';
+require_once __DIR__ . '/../controllers/UserController.php';
 
 $userController = new UserController();
 $userController->logout();
